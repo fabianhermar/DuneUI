ff5b89c3db79d5ab3e9cc4f38b7eb799f2403159
Fabian Herrera <contactofabianmartinez@gmail.com>
2025-08-01 21:38:22 -0600

    fix: update font classes in FAQ component for consistency

--- src/modules/sponsor/FAQ.tsx
+++ src/modules/sponsor/FAQ.tsx
@@ -13,12 +13,12 @@ export default function FAQ() {
 		>
 			<div className='mx-auto grid max-w-[40rem] grid-cols-1 gap-x-14 gap-y-16 lg:max-w-none lg:grid-cols-12'>
 				<div className='lg:col-span-4'>
-					<h2 className='text-lg leading-7 text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-GilroySemibold'>
+					<h2 className='text-lg leading-7 text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-display'>
 						Frequently asked questions
 					</h2>
-					<p className='mt-4 text-4xl font-GilroyBold tracking-tight text-black xl:text-6xl xl:leading-[3.5rem]'>
+					<p className='mt-4 text-4xl font-heading tracking-tight text-black xl:text-6xl xl:leading-[3.5rem]'>
 						Everything you need to know about wind UI
-						<span className='text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-GilroySemibold'>
+						<span className='text-transparent bg-clip-text bg-gradient-to-r from-[#93603e] via-[#c18156] to-[#cea472] font-display'>
 							.
 						</span>
 					</p>
@@ -28,8 +28,8 @@ export default function FAQ() {
 						<Accordion className='text-black'>
 							{FAQS.map((faq) => (
 								<AccordionItem
-									className='text-black font-GilroyRegular'
-									classNames={{ title: 'text-danger, font-GilroySemibold' }}
+									className='text-black font-accent'
+									classNames={{ title: 'text-danger, font-display' }}
 									key={faq.key}
 									aria-label={faq.label}
 									title={faq.title}
